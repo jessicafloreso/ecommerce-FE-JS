0aa390d03d6c5ec4eb64251fe124e9ea0edc4b7b
jessicafloreso <jessica.floresolguin@gmail.com>
2023-10-02 10:04:14 -0400

    this kinda works in terms of the way the sign in works to see. submissions still funky

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 // Define a shopping cart array to store products and their quantities
 const shoppingCart = [];
+const users = [
+    { id: 1, email: 'john@example.com', password: 'password123' },
+    // Add more user objects as needed
+];
 
 // Load the JSON data
 fetch('./products.json')
@@ -138,4 +142,115 @@ placeOrderButton.addEventListener('click', () => {
 });
 
 
+// JavaScript for toggling the profile options
+const profileLink = document.getElementById('profile-link');
+const profileOptions = document.getElementById('profile-options');
 
+profileLink.addEventListener('click', () => {
+    profileOptions.classList.toggle('active');
+});
+
+// Function to hide the profile options
+function hideProfileOptions() {
+    profileOptions.classList.remove('active');
+}
+
+// Attach an event listener to the close button
+const closeProfileOptionsButton = document.getElementById('close-profile-options');
+closeProfileOptionsButton.addEventListener('click', hideProfileOptions);
+
+// Attach a click event listener to the document to close the profile options when clicking outside
+document.addEventListener('click', (event) => {
+    const profileLink = document.getElementById('profile-link');
+    
+    if (!profileLink.contains(event.target) && !profileOptions.contains(event.target)) {
+        hideProfileOptions();
+    }
+});
+
+// JavaScript for toggling between login and register forms
+const showLoginButton = document.getElementById('show-login-button');
+const showRegisterButton = document.getElementById('show-register-button');
+const loginContent = document.getElementById('login-content');
+const registerContent = document.getElementById('register-content');
+const logoutAside = document.getElementById('logout-aside');
+
+showLoginButton.addEventListener('click', () => {
+    showLoginButton.classList.add('active');
+    showRegisterButton.classList.remove('active');
+    loginContent.style.display = 'block';
+    registerContent.style.display = 'none';
+    logoutAside.style.display = 'none'; // Hide logout options
+});
+
+showRegisterButton.addEventListener('click', () => {
+    showLoginButton.classList.remove('active');
+    showRegisterButton.classList.add('active');
+    loginContent.style.display = 'none';
+    registerContent.style.display = 'block';
+    logoutAside.style.display = 'none'; // Hide logout options
+});
+function registerUser() {
+    // Get user input (replace with actual form field values)
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const email = document.getElementById('register-email').value;
+    const password = document.getElementById('register-password').value;
+
+    // Create a new user object
+    const newUser = {
+        id: users.length + 1, // Assign a unique ID (you may use a more robust method in production)
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+    };
+
+    // Add the new user to the array
+    users.push(newUser);
+
+    // Simulate a successful registration (you may want to add error handling)
+    console.log('Registration successful:', newUser);
+
+    // Optionally, you can clear the registration form fields here
+    document.getElementById('first-name').value = '';
+    document.getElementById('last-name').value = '';
+    document.getElementById('register-email').value = '';
+    document.getElementById('register-password').value = '';
+
+    // After successful registration, you might want to automatically log in the user
+    // Call the loginUser function with appropriate data
+    loginUser(newUser.email, newUser.password);
+}
+const registerForm = document.getElementById('register-form');
+registerForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    registerUser();
+});
+
+function loginUser(email, password) {
+    // Get user input (replace with actual form field values)
+    const usernameInput = document.getElementById('login-email').value;
+    const passwordInput = document.getElementById('login-password').value;
+
+    // Check if there is a matching user in the predefined data
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if (user) {
+        // Simulate a successful login
+        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem('userName', user.username);
+
+        // Show the user's name and logout button
+        const userNameSpan = document.getElementById('user-name');
+        userNameSpan.textContent = user.username;
+        logoutAside.classList.add('logged-in');
+
+        // Hide the login and registration forms
+        loginContent.style.display = 'none';
+        registerContent.style.display = 'none';
+    } else {
+        // Display an error message or handle failed login
+        console.error('Login failed: Invalid username or password.');
+    }
+}
